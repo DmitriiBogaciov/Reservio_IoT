ce13fcfa1dea39e3931b4f7e72238dcceaf10eeb
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2024-03-12 11:21:36 +0100

    First commit

--- src/application.c
+++ src/application.c
@@ -14,11 +14,34 @@ twr_button_t button;
 twr_tmp112_t tmp112;
 uint16_t button_click_count = 0;
 
+void twr_set_led(uint64_t *id, const char *topic, void *value, void *param){
+
+	int led_value = *((int *)value);
+
+	switch (led_value){
+		case 0: 
+			twr_led_set_mode(&led, TWR_LED_MODE_BLINK_FAST);
+			break;
+		case 1: 
+			twr_led_set_mode(&led, TWR_LED_MODE_OFF);
+			break;
+		default: 
+			break;
+	}
+	
+	twr_log_info("State: %d", led_value);
+}
+
+static const twr_radio_sub_t subs[] = {
+    {"led/-/state/set", TWR_RADIO_SUB_PT_INT, twr_set_led, NULL}
+};
+
 // Button event callback
 void button_event_handler(twr_button_t *self, twr_button_event_t event, void *event_param)
 {
     // Log button event
     twr_log_info("APP: Button event: %i", event);
+    twr_led_set_mode(&led, TWR_LED_MODE_OFF);
 
     // Check event source
     if (event == TWR_BUTTON_EVENT_CLICK)
@@ -35,7 +58,7 @@ void button_event_handler(twr_button_t *self, twr_button_event_t event, void *ev
 void tmp112_event_handler(twr_tmp112_t *self, twr_tmp112_event_t event, void *event_param)
 {
     if (event == TWR_TMP112_EVENT_UPDATE)
-    {
+    {   
         float celsius;
         // Read temperature
         twr_tmp112_get_temperature_celsius(self, &celsius);
@@ -65,20 +88,19 @@ void application_init(void)
     twr_tmp112_set_event_handler(&tmp112, tmp112_event_handler, NULL);
     twr_tmp112_set_update_interval(&tmp112, 10000);
 
-    // Initialize radio
-    twr_radio_init(TWR_RADIO_MODE_NODE_SLEEPING);
-    // Send radio pairing request
-    twr_radio_pairing_request("skeleton", FW_VERSION);
+    twr_radio_init(TWR_RADIO_MODE_NODE_LISTENING);
+	twr_radio_set_subs((twr_radio_sub_t*)subs, sizeof(subs) / sizeof(twr_radio_sub_t));
+	twr_radio_pairing_request("demo", FW_VERSION);
 }
 
 // Application task function (optional) which is called peridically if scheduled
-void application_task(void)
-{
-    static int counter = 0;
+// void application_task(void)
+// {
+//     static int counter = 0;
 
-    // Log task run and increment counter
-    twr_log_debug("APP: Task run (count: %d)", ++counter);
+//     // Log task run and increment counter
+//     twr_log_debug("APP: Task run (count: %d)", ++counter);
 
-    // Plan next run of this task in 1000 ms
-    twr_scheduler_plan_current_from_now(1000);
-}
+//     // Plan next run of this task in 1000 ms
+//     twr_scheduler_plan_current_from_now(1000);
+// }
